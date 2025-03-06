978222511b53df29894e9713e7f00e53e29057f8
zack-young-ideas <zackyoung8709@gmail.com>
2025-03-06 02:54:59 -0500

    Define more realistic test data
    A new file was created that contains more realistic data to
    use when testing the app.

--- pages/api/assignments.ts
+++ pages/api/assignments.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import assignments from '../../testData';
+import assignments from '../../lib/placeholder-data';
 
 type Data = {
   greeting: string;
