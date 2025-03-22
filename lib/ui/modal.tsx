d146047e80f90b954ec07eaa2b0883f2c09c95bc
zack-young-ideas <zackyoung8709@gmail.com>
2025-03-21 23:43:16 -0400

    Add button to modal to close modal window

--- lib/ui/modal.tsx
+++ lib/ui/modal.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
-export default function Modal({ display }: { display: boolean }) {
+export default function Modal({
+  display, changeDisplay,
+}: {
+  display: boolean, changeDisplay: Function
+}) {
   return (
     <div
       style={{ display: display ? 'block' : 'none' }}
       data-testid="columns-modal"
     >
+      <button onClick={changeDisplay}>X</button>
       <h2>Columns</h2>
     </div>
   );
