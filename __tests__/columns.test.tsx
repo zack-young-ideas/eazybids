99853ea86361f48965811e1a97939650b2a96dbd
Zack Young <zyoung612@gmail.com>
2025-03-21 23:19:19 -0400

    Add Columns button to display columns modal
    A new button was added that displays a modal window. This modal
    will eventually contain checkboxes that enable the user to select
    which columns are displayed.

--- __tests__/columns.test.tsx
+++ __tests__/columns.test.tsx
@@ -5,7 +5,7 @@ Tests to ensure the user can change the columns that are displayed.
 import { http, HttpResponse } from 'msw';
 import { setupServer } from 'msw/node';
 
-import { render, within, screen } from '@testing-library/react';
+import { render, fireEvent, within, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import Home from '../pages/index';
@@ -35,5 +35,22 @@ describe('Home', () => {
     expect(columns[2]).toHaveTextContent('Avg. Weekly Hours');
     expect(columns[3]).toHaveTextContent('Avg. Weekly Pay');
     expect(columns[4]).toHaveTextContent('On-Duty Location');
+  });
+
+  it('displays columns modal when Columns button is clicked', async () => {
+    render(<Home />)
+
+    expect(screen.getByTestId('columns-modal')).not.toBeVisible();
+
+    const columnsButton = await screen.getByRole(
+      'button',
+      { name: /Columns/},
+    );
+
+    fireEvent.click(columnsButton);
+
+    const columnsModal = await screen.getByTestId('columns-modal');
+
+    expect(columnsModal).toBeVisible();
   });
 });
