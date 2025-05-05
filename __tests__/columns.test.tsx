576b72d5164241ecbb83bf630e9b58712bdfda29
Zack Young <zyoung612@gmail.com>
2025-05-05 01:12:21 -0400

    Refactor test to another file
    One of the end-to-end tests was moved to another file. This
    will help to better organize the tests.

--- __tests__/columns.test.tsx
+++ __tests__/columns.test.tsx
@@ -24,21 +24,6 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 describe('Home', () => {
-  it('displays default columns', async () => {
-    render(<Home />)
-
-    const table = await screen.findByRole('table')
-    const thead = within(table).getAllByRole('rowgroup')[0];
-    const columns = within(thead).getAllByRole('columnheader');
-
-    expect(columns).toHaveLength(5);
-    expect(columns[0]).toHaveTextContent(/^$/);
-    expect(columns[1]).toHaveTextContent('Number');
-    expect(columns[2]).toHaveTextContent('Avg. Weekly Hours');
-    expect(columns[3]).toHaveTextContent('Avg. Weekly Pay');
-    expect(columns[4]).toHaveTextContent('On-Duty Location');
-  });
-
   it('displays columns modal when Columns button is clicked', async () => {
     render(<Home />)
     const columnsModal = await screen.getByTestId('columns-modal');
